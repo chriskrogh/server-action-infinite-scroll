794bec6eb69a68cce02afdd800605cce31d52c2d
Chris Krogh <christopher.krogh@faire.com>
2024-08-05 18:06:57 -0400

    some heavy UI component

--- app/ListItem.tsx
+++ app/ListItem.tsx
@@ -9,8 +9,9 @@ type Props = {
 
 export const ListItem: React.FC<Props> = ({ item }) => {
   return (
-    <div className="flex gap-4 items-center w-full h-[200px] p-2 border-2 border-red-500">
+    <div className="flex justify-between items-center w-full h-[200px] p-2 border-2 border-red-500">
       <p>{item}</p>
+      <p>Some heavy UI component</p>
       <InvalidateButton />
     </div>
   );
